22b480c8d31a384187cf4d687826f773ab6785a5
Bo Hu <bohu01@gmail.com>
2012-08-26 00:57:14 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Web1::Application.config.secret_token = 'b2c157d9f1bd92616e996b737896090fccd3d3d4c6e58eaf2ea6837d060972de77af6c85647f46af37f1d02ff91c690b6e2e8820741e2d84252a90dd4a00b0b0'
